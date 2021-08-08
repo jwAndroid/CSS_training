a62e4d7ee5a10a38900c145ba1741c2d58495016
JwAndroid <cjd9408er@naver.com>
2021-08-08 15:01:13 +0900

    Update quiz

--- 01_glam_clone/script/data.js
+++ 01_glam_clone/script/data.js
@@ -1,34 +1,42 @@
 const data = [
   {
-    questions: ["당신의 코딩", "실력은?"],
-    answers: ["잘한다.", "못한다.", "중간정도 한다."],
+    questions: ["문제1", "var를 써야될까요? 쓰지말아야 할까요??"],
+    answers: ["쓴다", "안쓴다", "모른다"],
   },
   {
-    questions: ["HTML , CSS", "에 대한 지식을 알고있습니까?"],
-    answers: ["예", "아니요"],
+    questions: ["스코프중 함수범위 인것은?"],
+    answers: ["let", "const", "var"],
   },
   {
-    questions: ["HTML , CSS", "문제1"],
-    answers: ["예", "아니요"],
+    questions: ["멘탈 나갈만한것은??"],
+    answers: ["리덕스", "리덕스 사가", "안드로이드"],
   },
   {
-    questions: ["HTML , CSS", "3"],
-    answers: ["예", "아니요"],
+    questions: ["funtion 이 나왔다. 왜 썻을까?"],
+    answers: ["this 가 있어서", "그냥", "익숙해서", "리덕스 사가를 위해서"],
   },
   {
-    questions: ["HTML , CSS", "4"],
-    answers: ["예", "아니요"],
+    questions: ["map , filter , reduce", "가 나왔다 중요할까?"],
+    answers: ["중요하다", "중요하지 않다", "너무 중요하다"],
   },
   {
-    questions: ["HTML , CSS", "5"],
-    answers: ["예", "아니요"],
+    questions: ["var를 썻다 어떻게 해야할까?"],
+    answers: ["냅둔다", "let으로 고친다", "const를 쓴다", "상황에따라 다르다"],
   },
   {
-    questions: ["HTML , CSS", "5"],
-    answers: ["예", "아니요"],
+    questions: ["(x && y ) 앞에가 참이면?"],
+    answers: ["y가 실행된다.", "실행되지 않는다."],
   },
   {
-    questions: ["HTML , CSS", "5"],
-    answers: ["예", "아니요"],
+    questions: ["(x && y ) 앞에가 거짓이면?"],
+    answers: ["y가 실행된다.", "실행되지 않는다."],
+  },
+  {
+    questions: ["(x || y ) 앞에가 참이면?"],
+    answers: ["y가 실행된다.", "실행되지 않는다."],
+  },
+  {
+    questions: ["(x || y ) 앞에가 거짓이면?"],
+    answers: ["y가 실행된다.", "실행되지 않는다."],
   },
 ];
--- 01_glam_clone/script/result.js
+++ 01_glam_clone/script/result.js
@@ -1,7 +1,10 @@
 const resultContainer = document.querySelector(".result-container");
+const correctContainer = document.querySelector(".correct-container");
 const point = document.querySelector(".contents");
 
 const resultPoints = 0;
+const arr = [];
+const correctArr = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 const button = document.querySelector(".button");
 button.addEventListener("click", () => {
@@ -9,13 +12,41 @@ button.addEventListener("click", () => {
 });
 
 const resultPoint = () => {
-  const resultData = localStorage.getItem("results");
-  const results = JSON.parse(resultData);
+  const results = JSON.parse(localStorage.getItem("results"));
 
-  console.log(results);
+  const resultMap = results.map((result) => {
+    return arr.push(result.answer);
+  });
 
-  resultContainer.innerHTML = results;
+  resultContainer.innerHTML = arr;
+  correctContainer.innerHTML = correctArr;
   point.innerHTML = resultPoints + "점";
+
+  compareCorrect(correctArr, arr);
+
+  console.log("정답 어레이 " + correctArr);
+  console.log("쓴거 " + arr);
+};
+
+// const a = ["1", "2", "3", "4", "5"];
+// const b = ["2", "4", "5", "7", "8"];
+
+const compareCorrect = (correctArr, arr) => {
+  // a.map((x, index) => {
+  //   b.map((y, index) => {
+  //     if (x[index] == y[index]) {
+  //       console.log(`${x[index]}`);
+  //     }
+  //   });
+  // });
+  for (let i = 0; i < correctArr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (correctArr[i] == arr[j]) {
+        // console.log(`${a[i]}`);
+        console.log(a[i].length);
+      }
+    }
+  }
 };
 
 resultPoint();
