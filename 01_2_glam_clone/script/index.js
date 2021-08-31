c0e9d8d542366e24500fd5986179a0995bb30cc5
jwAndroid <cjd9408er@naver.com>
2021-08-31 20:22:15 +0900

    Study 0831

---
+++ 01_2_glam_clone/script/index.js
@@ -0,0 +1,10 @@
+const button1 = document.querySelector('.button.button1-js');
+const button2 = document.querySelector('.button.button2-js');
+
+button1.addEventListener('click', () => {
+  location.href = './test.html';
+});
+
+button2.addEventListener('click', () => {
+  location.href = './test.html';
+});
